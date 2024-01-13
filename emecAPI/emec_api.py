29529fd3cb21bc325a9ceb07d84ccb763714d71a
Natanael Maia <contato@natanael.dev.br>
2024-01-13 11:55:12 -0300

    Add logging, json, asyncio, aiohttp, pathlib imports and update EmecAPI class

--- emecAPI/emec_api.py
+++ emecAPI/emec_api.py
@@ -1,6 +1,6 @@
 from .ies    import IesAPI
-import aiohttp
-import logging
+import logging, json
+import asyncio, aiohttp, pathlib
 
 class EmecAPI:
     def __init__(self, session=None, auto_add_ies=True):
@@ -28,7 +28,8 @@ class EmecAPI:
         Returns:
             str: A string representation of the EmecAPI instance.
         """
-        return f"EmecAPI(institutions={self.institutions})"
+        ies = ', '.join([str(ies_id) for ies_id in self.institutions])
+        return f"EmecAPI(institutions = {ies})"
 
     def __setup_logger(self) -> None:
         """
@@ -84,7 +85,7 @@ class EmecAPI:
         if ies_id in self.institutions:
             del self.institutions[ies_id]
 
-    async def to_dict(self):
+    def to_dict(self):
         """
         Converts the EmecAPI instance to a dictionary representation.
 
@@ -92,6 +93,71 @@ class EmecAPI:
             dict: A dictionary representation of the EmecAPI instance, where the keys are the institution (IES) IDs
                 and the values are the corresponding institution (IES) objects converted to dictionaries.
         """
-        print('Converting to dict')
-        print(self.institutions)
+        data = asyncio.run(self.to_dict_async())
+        return data
+
+    async def to_dict_async(self):
+        """
+        Converts the EmecAPI instance to a dictionary representation asynchronously.
+
+        Returns:
+            dict: A dictionary representation of the EmecAPI instance, where the keys are the institution (IES) IDs
+                and the values are the corresponding institution (IES) objects converted to dictionaries.
+        """
         return {ies_id: await ies.to_dict() for ies_id, ies in self.institutions.items()}
+
+    def to_json(self, file='EMEC_API.json', indent=4, ensure_ascii=False, encoding='utf-8', **kwargs):
+        """
+        Converts the EmecAPI instance to JSON format and writes it to a file.
+
+        Args:
+            file (str): The path to the output JSON file.
+            indent (int): The number of spaces used for indentation in the JSON file. Defaults to 4.
+            ensure_ascii (bool): If True, non-ASCII characters will be escaped in the JSON file. Defaults to False.
+            **kwargs: Additional keyword arguments to be passed to the json.dumps() function.
+
+        Returns:
+            None
+        """
+        asyncio.run(self.to_json_async(file=file, indent=indent, ensure_ascii=ensure_ascii, **kwargs))
+
+    async def to_json_async(self, file='EMEC_API.json', indent=4, ensure_ascii=False, encoding='utf-8', **kwargs):
+        """
+        Converts the object to JSON format and writes it to a file asynchronously.
+
+        Args:
+            file (str): The path to the output JSON file. Defaults to 'EMEC_API.json'.
+            indent (int): The number of spaces used for indentation in the JSON file. Defaults to 4.
+            ensure_ascii (bool): If True, non-ASCII characters will be escaped in the JSON file. Defaults to False.
+            **kwargs: Additional keyword arguments to be passed to the json.dumps() function.
+
+        Returns:
+            None
+        """
+        pathlib.Path(file).parent.mkdir(parents=True, exist_ok=True)
+        file = pathlib.Path(file).absolute()
+
+        with open(file, 'w', encoding=encoding) as f:
+            f.write(json.dumps(await self.to_dict_async(), indent=indent, ensure_ascii=ensure_ascii, **kwargs))
+
+    @classmethod
+    def main(cls, ies_ids, method:list = []):
+        """
+        Executes the main logic of the EMEC API. This method is for example purposes only.
+        For a better usage, it is recommended to use the EmecAPI class directly in your code.
+
+        Args:
+            ies_ids (list): A list of IES IDs.
+            method (str): The method to be used.
+
+        Returns:
+            EMECAPI: An instance of the EMECAPI class.
+        """
+        async def async_main():
+            async with aiohttp.ClientSession() as session:
+                api = cls(session=session, auto_add_ies=True)
+                tasks = [asyncio.create_task(api.get_ies(ies_id, methods=method)) for ies_id in ies_ids]
+                await asyncio.gather(*tasks)
+                return api
+
+        return asyncio.run(async_main())
